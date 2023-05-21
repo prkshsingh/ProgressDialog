a1da4e3870b0b1ee2ef4ff78604fedca78e36df5
Prakash Singh <prksh.singh23@gmail.com>
2023-05-21 23:28:33 +0530

    Commit

---
+++ app/src/main/java/com/prksh/progressdialog/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.prksh.progressdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private val progressDialog by lazy { CustomProgressDialog(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start_btn)
+        startButton.setOnClickListener {
+            // Show progress dialog
+            progressDialog.start("Please Wait...")
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                // Dismiss progress bar after 4 seconds
+                progressDialog.stop()
+            }, 4000)
+        }
+    }
+}
